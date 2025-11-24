ed1578e1cf99b517fd019b54e6ba49f2ae07b705
Meghan <meghantomback@gmail.com>
2025-11-23 20:16:22 -0500

    class and constructors

--- CSO/CSO/Program.cs
+++ CSO/CSO/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace CSO
 {
+    public class Character
+    {
+        public string name;
+        public int exp;
+
+        public Character()
+        {
+            name = "Not assigned";
+        }
+
+        public Character(string name)
+        {
+            this.name = name;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Character hero = new Character();
+            Console.WriteLine("Hero: " + hero.name + " - " + hero.exp + " EXP");
+
+            Character heroine = new Character("Agatha");
+            Console.WriteLine("Hero: " + heroine.name + " - " + heroine.exp + " EXP");
+
         }
     }
 }
